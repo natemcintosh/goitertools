dcfe7cd4f1b1fc92bf98698ee9e7f90e3465dc19
Nate McIntosh <40805156+natemcintosh@users.noreply.github.com>
2023-01-08 12:58:58 -0500

    tests for Accumulate

--- ends_on_shortest_input_test.go
+++ ends_on_shortest_input_test.go
@@ -95,3 +95,90 @@ func ExampleAccumulateWithInit() {
 	// -15
 
 }
+
+func TestAccumulateAdd(t *testing.T) {
+	c := make(chan int)
+
+	add := func(a, b int) int { return a + b }
+	go Accumulate([]int{1, 2, 3, 4, 5}, add, c)
+
+	want := []int{1, 3, 6, 10, 15}
+
+	idx := 0
+	for v := range c {
+		assert.Equal(t, want[idx], v)
+		idx += 1
+	}
+}
+
+func BenchmarkAccumulateAdd(b *testing.B) {
+	c := make(chan int)
+	add := func(a, b int) int { return a + b }
+	go Accumulate([]int{1, 2, 3, 4, 5}, add, c)
+
+	for i := 0; i < b.N; i++ {
+		<-c
+	}
+}
+
+func TestAccumulateAdd2(t *testing.T) {
+	c := make(chan int)
+
+	add := func(a, b int) int { return a + b }
+	go Accumulate([]int{1, 2, 3, 4, 5}, add, c)
+
+	want := []int{1, 3, 6, 10, 15}
+
+	idx := 0
+	for v := range c {
+		assert.Equal(t, want[idx], v)
+		idx += 1
+	}
+}
+
+func TestAccumulateMax(t *testing.T) {
+	c := make(chan float64)
+
+	go Accumulate([]float64{1, 2, 3, 4, 5}, math.Max, c)
+
+	want := []float64{1, 2, 3, 4, 5}
+
+	idx := 0
+	for v := range c {
+		assert.Equal(t, want[idx], v)
+		idx += 1
+	}
+}
+
+func TestAccumulateMin(t *testing.T) {
+	c := make(chan float64)
+	go Accumulate([]float64{1, 2, 3, 4, 5}, math.Min, c)
+
+	want := []float64{1, 1, 1, 1, 1}
+
+	idx := 0
+	for v := range c {
+		assert.Equal(t, want[idx], v)
+		idx += 1
+	}
+}
+
+func ExampleAccumulate() {
+	c := make(chan int)
+
+	// Using a non-cummutative function, i.e. a-b != b-a
+	sub := func(a, b int) int { return a - b }
+	data := []int{5, 4, 3, 2, 1}
+	// The first item sent from AccumulateWithInit will be `initial`
+	go Accumulate(data, sub, c)
+
+	for v := range c {
+		fmt.Printf("%v\n", v)
+	}
+	//Output: 5
+	// 1
+	// -2
+	// -4
+	// -5
+
+}
